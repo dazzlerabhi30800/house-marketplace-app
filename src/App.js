75335553f67391a939d049a4d628ced683874525
= <abhisingh30800@gmail.com>
2021-12-23 19:56:29 +0530

    Navbar Component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
 import './App.css';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import Navbar from './components/Navbar';
+import Explore from './pages/Explore'
+import ForgotPassword from './pages/ForgotPassword';
+import Offers from './pages/Offers'
+import Profile from './pages/Profile'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
 
 function App() {
   return (
     <>
-      <h1>My App</h1>
+      <Router>
+        <Routes>
+          <Route exact path='/' element={<Explore />} />
+          <Route exact path='/offers' element={<Offers />} />
+          <Route exact path='/profile' element={<SignIn />} />
+          <Route exact path='/sign-in' element={<SignIn />} />
+          <Route exact path='/sign-up' element={<SignUp />} />
+          <Route exact path='/forgot-password' element={<ForgotPassword />} />
+        </Routes>
+        <Navbar />
+      </Router>
+
     </>
   );
 }
