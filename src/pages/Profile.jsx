d64871269a2412a37e0f21ccc91ad0fe7c799655
= <abhisingh30800@gmail.com>
2021-12-24 12:19:25 +0530

    User logout

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,17 +1,30 @@
 import {useEffect, useState} from 'react'
 import {getAuth} from 'firebase/auth'
-
+import {useNavigate, Link} from 'react-router-dom'
 
 function Profile() {
-    const [user, setUser] = useState(null)
-
     const auth = getAuth()
-    useEffect(() => {
-        setUser(auth.currentUser)
-        //eslint-disable-next-line
-    }, [])
+    const [formData, setFormData] = useState({
+        name : auth.currentUser.displayName,
+        email : auth.currentUser.email,
+    })
 
-    return user ? <h1>{user.displayName}</h1> : 'Not logged in!'
+    const {name, email} = formData
+    const navigate = useNavigate()
+    const onLogout = () => {
+        auth.signOut()
+        navigate('/')
+    }
+
+
+    return <div className='profile'>
+        <header className="profileHeader">
+            <div className="pageHeader">My Profile</div>
+            <button onClick={onLogout} type='button' className="logOut">
+                Logout
+            </button>
+        </header>
+    </div>
 }
 
 export default Profile
