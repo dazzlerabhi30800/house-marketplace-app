34d78681d6d2ce3d2224d2f117fac6a76734c0f2
= <abhisingh30800@gmail.com>
2021-12-25 18:00:19 +0530

    Explorer Page

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import PrivateRoute from './components/PrivateRoute';
 import Explore from './pages/Explore'
 import ForgotPassword from './pages/ForgotPassword';
 import Offers from './pages/Offers'
+import Category from './pages/Category';
 import Profile from './pages/Profile'
 import SignIn from './pages/SignIn'
 import SignUp from './pages/SignUp'
@@ -18,6 +19,7 @@ function App() {
         <Routes>
           <Route exact path='/' element={<Explore />} />
           <Route exact path='/offers' element={<Offers />} />
+          <Route exact path='/category/:categoryName' element={<Category />} />
           <Route exact path='/profile' element={<PrivateRoute />} >
             <Route exact path='/profile' element={<Profile />} />
           </Route>
